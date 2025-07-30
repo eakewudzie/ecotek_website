2c18ebb7fb03e9c82bcf11312e3b017b88e264c6
Sonnysam <samuelagbenyo067@gmail.com>
2025-07-29 19:36:23 -0700

    eunice added

---
+++ app/eunice/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Eunice = () => {
+    return (
+        <div className='flex bg-red-400 h-screen justify-center items-center'>
+            <p className='text-9xl font-bold'>Eunice is a good girl</p>
+        </div>
+    )
+}
+
+export default Eunice
